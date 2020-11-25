17da0d2ae33d6d546ff1fca53ad43da01643e7b3
alanchang920 <73819875+alanchang920@users.noreply.github.com>
2020-11-25 20:46:05 +0800

    Update app.js
    modify mongoose config

--- app.js
+++ app.js
@@ -1,20 +1,10 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
-const mongoose = require('mongoose')
 const app = express()
 const port = 3000
 
-mongoose.connect('mongodb://localhost/expense', { useNewUrlParser: true, useUnifiedTopology: true })
 
-const db = mongoose.connection
-
-db.on('error', () => {
-  console.log('mongodb error!')
-})
-
-db.once('open', () => {
-  console.log('mongodb connected!')
-})
+require('./config/mongoose')
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
