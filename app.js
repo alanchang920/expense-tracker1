a66325523e85897c5524350692c0666ebbf98e17
alanchang920 <73819875+alanchang920@users.noreply.github.com>
2020-11-26 02:05:06 +0800

    Update app.js

--- app.js
+++ app.js
@@ -6,14 +6,9 @@ const port = 3000
 
 const Category = require('./models/category')
 const Record = require('./models/record')
-const totalCalc = function calcTotal(options) {
-  let totalAmount = 0
-  options.forEach((item) => {
-    totalAmount += Number(item.amount)
-  })
+const totalCalc = require('./public/javascripts/totalCalc')
+const getIcon = require('./public/javascripts/iconType')
 
-  return totalAmount
-}
 
 require('./config/mongoose')
 
@@ -23,27 +18,47 @@ app.set('view engine', 'handlebars')
 app.use(bodyParser.urlencoded({ extended: true }))
 
 app.get('/', (req, res) => {
-  // 取出 category model 所有資料
+
   Category.find()
-    .lean()  // 把 Mongoose 的 Model 物件轉換成乾淨的 JavaScript 資料陣列
-    .sort({ _id: 'asc' })  // 排序(順)
+    .lean()
+    .sort({ _id: 'asc' })
     .then((categoryies) => {
       categoryData = categoryies
     })
     .then(() => {
-      // 取出 Record model 所有資料
+
       return Record.find()
-        .lean()  // 把 Mongoose 的 Model 物件轉換成乾淨的 JavaScript 資料陣列
-        .sort({ date: 'desc' })  // 排序(反)
+        .lean()
+        .sort({ date: 'desc' })
         .then((records) => {
-          // 將資料傳給 index 樣板
+
           return res.render('index', { records, categoryies: categoryData, totalAmount: totalCalc(records) })
         })
-        .catch((error) => console.log(error))  // 錯誤處理  
+        .catch((error) => console.log(error))
     })
-    .catch((error) => console.log(error))  // 錯誤處理  
+    .catch((error) => console.log(error))
 })
 
+app.get('/records/new', (req, res) => {
+  Category.find()
+    .lean()
+    .sort({ _id: 'asc' })
+    .then((categoryies) => {
+      categoryData = categoryies
+      return res.render('new', { categoryies: categoryData })
+    })
+    .catch((error) => console.log(error))
+})
+
+app.post('/', (req, res) => {
+  const options = req.body
+  options.icon = getIcon(req.body.category)
+  options.showDate = options.date.replace(/-/g, '/')
+  const recordAddNew = new Record(options)
+  return recordAddNew.save()
+    .then(() => res.redirect('/'))
+    .catch((error) => console.log(error))
+})
 
 app.listen(port, () => {
   console.log(`Express in listening on http://localhost:${port}`)
